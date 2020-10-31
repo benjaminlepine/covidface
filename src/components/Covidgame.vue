5ee33fca1938565d67e9a6a1f7f5d12bed628e2b
Benjamin <benjamin.lepine@epitech.eu>
2020-10-31 20:06:33 +0100

    animation virus timeout ok

--- src/components/Covidgame.vue
+++ src/components/Covidgame.vue
@@ -4,7 +4,9 @@
     <div class="starface">
 
       <img class="starface--img" src="../assets/leonardodicaprio.png" alt="">
-      <img class="starface--mask" src="../assets/mask2.png" alt="">
+      <img class="starface--mask" v-if="displayMask" src="../assets/mask2.png" alt="">
+<!--      <img class="starface&#45;&#45;aswitem" v-if="!displayMask && seringue" src="../assets/seringue.png" alt="">-->
+      <img class="starface--aswitem" v-if="!displayMask && virus" src="../assets/virus.png" alt="">
 
       <div>
         <div class="answers">
@@ -34,9 +36,16 @@
         answerTwo: 'Jean Dujardin',
         answerThree: 'Matt Damon',
         answerFour: 'Georges Clooney',
+        displayMask: true,
+        virus: false,
+        seringue: true
       }
     },
     methods : {
+      displayAnimation(){
+        this.virus = false;
+        /* TODO Load new image && reload component */
+      },
       sendAnswer(answer){
         // axios.post('/your-url', {name: this.name})
         //         .then(res => {
@@ -44,7 +53,10 @@
         //         })
         //         .catch(err => {// catch error});
         //         }
-        console.log('Your answer is = ', answer)
+        console.log('Your answer is = ', answer);
+        this.displayMask = false;
+        this.virus = true;
+        setTimeout(function () { this.displayAnimation() }.bind(this), 600);
       }
 
     }
@@ -67,9 +79,16 @@
     &--mask{
       position: absolute;
       left: 50%;
-      margin-left: -151px;
+      margin-left: -153px;
       top: 37%;
       max-width: 320px;
+    }
+    &--aswitem{
+      position: absolute;
+      left: 50%;
+      margin-left: -60px;
+      top: 67%;
+      max-width: 120px;
     }
     .answers {
       display: flex;
@@ -87,6 +106,9 @@
         margin: 4px 4px;
         width: 100%;
         max-width: 208px;
+        &:hover{
+          opacity: 0.8;
+        }
       }
     }
   }
@@ -95,6 +117,5 @@
   @media screen and (max-width: 350px){.starface--mask{max-width: 240px;margin-left:-115px;top: 35%;}}
   @media screen and (max-width: 300px){.starface--mask{max-width: 200px;margin-left:-95px;top: 34%;}}
   @media screen and (max-width: 250px){.starface--mask{max-width: 170px;margin-left:-82px;top: 33%;}}
-
 
 </style>
