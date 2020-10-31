d0d33c3ddadd79abecff3295a3cd5e3302f54fe4
Benjamin <benjamin.lepine@epitech.eu>
2020-10-31 19:08:31 +0100

    pixel perfect alignment mask

--- src/components/Covidgame.vue
+++ src/components/Covidgame.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="game-ctn">
+  <div>
     <div class="hello"></div>
-      <div class="starface">
-        <img class="starface--img" src="../assets/leonardodicaprio.png" alt="">
-        <div>
+    <div class="starface">
+
+      <img class="starface--img" src="../assets/leonardodicaprio.png" alt="">
+      <img class="starface--mask" src="../assets/mask2.png" alt="">
+
+      <div>
         <div class="answers">
           <button v-on:click="sendAnswer(answerOne)" class="answers--choice">{{answerOne}}</button>
           <button v-on:click="sendAnswer(answerTwo)" class="answers--choice">{{answerTwo}}</button>
@@ -12,9 +15,9 @@
           <button v-on:click="sendAnswer(answerThree)" class="answers--choice">{{answerThree}}</button>
           <button v-on:click="sendAnswer(answerFour)" class="answers--choice">{{answerFour}}</button>
         </div>
-        </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -46,16 +49,13 @@
 
     }
 
-}
+  }
 </script>
 
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
-  .game-ctn{
-    max-height: 100%;
-  }
   .starface{
     position: relative;
     &--img{
@@ -64,23 +64,37 @@
       max-width: 95%;
       max-height: 700px;
     }
-  .answers {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    &--mask{
+      position: absolute;
+      left: 50%;
+      margin-left: -151px;
+      top: 37%;
+      max-width: 320px;
+    }
+    .answers {
+      display: flex;
+      justify-content: center;
+      align-items: center;
 
-    &--choice {
-      min-height: 60px;
-      padding: 6px 20px;
-      background-color: #2c3e50;
-      color: white;
-      font-size: 18px;
-      border-radius: 16px;
-      border: none;
-      margin: 4px 4px;
-      width: 100%;
-      max-width: 208px;
+      &--choice {
+        min-height: 60px;
+        padding: 6px 20px;
+        background-color: #2c3e50;
+        color: white;
+        font-size: 18px;
+        border-radius: 16px;
+        border: none;
+        margin: 4px 4px;
+        width: 100%;
+        max-width: 208px;
+      }
     }
   }
-  }
+  @media screen and (max-width: 450px){.starface--mask{max-width: 295px;margin-left:-140px;}}
+  @media screen and (max-width: 400px){.starface--mask{max-width: 270px;margin-left:-130px;}}
+  @media screen and (max-width: 350px){.starface--mask{max-width: 240px;margin-left:-115px;top: 35%;}}
+  @media screen and (max-width: 300px){.starface--mask{max-width: 200px;margin-left:-95px;top: 34%;}}
+  @media screen and (max-width: 250px){.starface--mask{max-width: 170px;margin-left:-82px;top: 33%;}}
+
+
 </style>
