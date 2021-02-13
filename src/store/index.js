5b638138ec9d38967ddef2f6576c472585d8e568
Mina Amrouche <mina.amrouche9@gmail.com>
2021-02-13 19:18:14 +0100

    Format store file

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
@@ -8,10 +8,7 @@ export default new Vuex.Store({
     score: 0,
     percent: 100,
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
