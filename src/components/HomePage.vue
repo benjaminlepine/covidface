d59f2dc673fad74f36b13ce6715209bb34723824
Mina Amrouche <mina.amrouche9@gmail.com>
2021-02-13 15:47:28 +0100

    Remove hourglass from homepage and format file

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,14 +1,18 @@
 <template>
-    <div class="home-ctn pt-3">
-<!--        <Hourglass/>-->
-        <img class="home-macron" src="../assets/macron.png" alt="">
-        <h1 class="home-title mt-4">COVID-FACE</h1>
-        <div class="home-subtitle">
-            <h2>Trouvez quelle star se cache sous le masque.</h2>
-            <p>Jouez au quiz du coronavirus et identifiez les stars masquées. Serez-vous capable d'obtenir le meilleur score</p>
-        </div>
-        <router-link to="/Covidgame" class="btn covid-btn covid-btn--start">Commencer</router-link><br>
-        <!--<div class="categorie-ctn mb-5">
+  <div class="home-ctn pt-3">
+    <img class="home-macron" src="../assets/macron.png" alt="" />
+    <h1 class="home-title mt-4">COVID-FACE</h1>
+    <div class="home-subtitle">
+      <h2>Trouvez quelle star se cache sous le masque.</h2>
+      <p>
+        Jouez au quiz du coronavirus et identifiez les stars masquées.
+        Serez-vous capable d'obtenir le meilleur score
+      </p>
+    </div>
+    <router-link to="/Covidgame" class="btn covid-btn covid-btn--start"
+      >Commencer</router-link
+    ><br />
+    <!--<div class="categorie-ctn mb-5">
           <h1>CATEGORIES</h1>
           <div class="row">
             <div class="categorie-element--ghost col"></div>
@@ -35,87 +39,85 @@
             <div class="categorie-element--ghost col"></div>
           </div>
         </div>-->
-    </div>
+  </div>
 </template>
 
 <script>
-    import Hourglass from '../animations/Hourglass'
-
-    export default {
-        components: { Hourglass },
-        name: 'HomePage',
-        props: {
-            msg: String
-        },
-        el : 'main',
-        data: function () {
-            return {}
-        },
-        methods : {}
-    }
+export default {
+  name: "HomePage",
+  props: {
+    msg: String,
+  },
+  el: "main",
+  data: function () {
+    return {};
+  },
+  methods: {},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-    @import "../style";
-    .home{
-        &-ctn{
-            padding-bottom: 20px;
-            min-height: 900px;
-        }
-        &-macron{max-width: 120px}
-        &-title{
-            color: #acd3f7;
-            font-size: 45px;
-            font-family: Coronaviral, Calibri, sans-serif;
-            margin-bottom: 0;
-        }
-        &-subtitle{
-            margin: 10px 0 10px 0;
-            color: #acd3f7 ;
-        }
+@import "../style";
+.home {
+  &-ctn {
+    padding-bottom: 20px;
+    min-height: 900px;
+  }
+  &-macron {
+    max-width: 120px;
+  }
+  &-title {
+    color: #acd3f7;
+    font-size: 45px;
+    font-family: Coronaviral, Calibri, sans-serif;
+    margin-bottom: 0;
+  }
+  &-subtitle {
+    margin: 10px 0 10px 0;
+    color: #acd3f7;
+  }
+}
 
+.categorie {
+  &-icn {
+    max-width: 100px;
+  }
+  &-ctn {
+    & h1 {
+      color: #5ff759;
+      font-size: 35px;
+      font-family: Coronaviral, "Times New Roman", Times, sans-serif;
     }
+    border: 2px solid #5ff759;
+    border-radius: 30px;
+    padding: 15px;
+    margin: 30px;
+  }
+  &-element {
+    transition: filter 300ms, opacity 300ms, transform 0.8s ease-in-out;
+    -webkit-transition: -webkit-transform 0.4s ease-in-out;
+    padding: 0px;
+    &:hover {
+      filter: brightness(1.25);
+      cursor: pointer;
+      -webkit-transform: rotate(10deg);
+      transform: rotate(10deg);
+      border-radius: 20%;
+      border-color: #536d8a;
+    }
+  }
+  &-subtitle {
+    color: #a2c2e3;
+    margin-bottom: 0;
+    font-size: 18px;
+    font-family: Helvetica;
+  }
+}
 
-    .categorie{
-        &-icn {
-            max-width: 100px;
-        }
-        &-ctn{
-            & h1{
-                color: #5ff759;
-                font-size: 35px;
-                font-family: Coronaviral, "Times New Roman", Times, sans-serif;
-            }
-            border: 2px solid #5FF759;
-            border-radius: 30px;
-            padding: 15px;
-            margin: 30px;
-        }
-        &-element{
-            transition: filter 300ms, opacity 300ms, transform .8s ease-in-out;
-            -webkit-transition: -webkit-transform .4s ease-in-out;
-            padding: 0px;
-            &:hover {
-                filter: brightness(1.25);
-                cursor: pointer;
-                -webkit-transform: rotate(10deg);
-                transform: rotate(10deg);
-                border-radius: 20%;
-                border-color: #536d8a;
-            }
-        }
-        &-subtitle{
-            color: #a2c2e3;
-            margin-bottom: 0;
-            font-size: 18px;
-            font-family: Helvetica;
-        }
-    }
-
-    @media screen and (max-width: 800px){
-        .categorie-element--ghost{
-            display: none;
-        }
-    }
+@media screen and (max-width: 800px) {
+  .categorie-element--ghost {
+    display: none;
+  }
+}
 </style>
