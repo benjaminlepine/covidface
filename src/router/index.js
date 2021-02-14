74e27955751314bc55d4114b0bea4063f2872858
Mina Amrouche <mina.amrouche9@gmail.com>
2021-02-14 18:03:13 +0100

    Format router file

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,31 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Covidgame from '../components/Covidgame';
-import Homepage from '../components/HomePage';
-import EndGame from '../components/EndGame';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Covidgame from "../components/Covidgame";
+import Homepage from "../components/HomePage";
+import EndGame from "../components/EndGame";
 
 Vue.use(VueRouter);
 
-
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/Home', component: Home },
-  { path: '/Covidgame', component: Covidgame},
-  { path: '/Homepage', component: Homepage},
-  { path: '/EndGame', component: EndGame},
-  { path: '/about', name: 'About', component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')},
-  { path: '*', component: Home }
-]
+  { path: "/", name: "Home", component: Home },
+  { path: "/Home", component: Home },
+  { path: "/Covidgame", component: Covidgame },
+  { path: "/Homepage", component: Homepage },
+  { path: "/EndGame", component: EndGame },
+  {
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  { path: "*", component: Home },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
